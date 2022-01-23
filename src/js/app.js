477bca41e4953a10898c2f9118b2a84d0c40102f
Vyacheslav <slava.89989@gmail.com>
2022-01-23 14:01:50 +0500

    Доделал версту с анимациями и js

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,52 @@ import * as flsFunctions from './modules/functions.js';
 
 flsFunctions.isWebp();
 
-import Swiper, { Navigation, Pagination } from 'swiper';
+let feedbackBtn = document.querySelector(".feedback__btn");
 
-const swiper = new Swiper();
+let feedbackAdultMinus = document.querySelector(".feedback__left .feedback__minus");
+let feedbackAdultPlus = document.querySelector(".feedback__left .feedback__plus");
+
+let feedbackChildMinus = document.querySelector(".feedback__right .feedback__minus");
+let feedbackChildPlus = document.querySelector(".feedback__right .feedback__plus");
+
+function openFeedback(e) {
+  e.preventDefault()
+  let popup = document.querySelector(".feedback__popup");
+
+  if (popup.classList.value === "feedback__popup") {
+    popup.classList.add("feedback__popup--active")
+  } else if (popup.classList.value === "feedback__popup feedback__popup--active") {
+    popup.classList.remove("feedback__popup--active")
+  }
+}
+
+feedbackAdultMinus.onclick = function() {
+  if (adult.value <= 0) {
+    feedbackAdultMinus.setAttribute("disabled", "disabled")
+  } else if(adult.value > 0) {
+    feedbackAdultMinus.removeAttribute("disabled")
+    adult.value--
+  }
+}
+
+feedbackAdultPlus.onclick = function() {
+  feedbackAdultMinus.removeAttribute("disabled")
+  adult.value++
+}
+
+feedbackChildMinus.onclick = function() {
+  if (child.value <= 0) {
+    feedbackChildMinus.setAttribute("disabled", "disabled")
+  } else if(child.value > 0) {
+    feedbackChildMinus.removeAttribute("disabled")
+    child.value--
+  }
+}
+
+
+feedbackChildPlus.onclick = function() {
+  feedbackChildMinus.removeAttribute("disabled")
+  child.value++
+}
+
+feedbackBtn.addEventListener("click", openFeedback)
